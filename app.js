ea20ce6d5fe575149f4bf5391c6e50a4205538cc
csm <csm009177@gamil.com>
2023-10-12 11:30:05 +0900

    restore

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http')
 const server = http.createServer((req,res) => {
   res.writeHead(200, {'Content-Type' : 'text/plain'});
   res.write('향마루');
-  // res.end();
+  res.end();
 });
 
 server.listen(3500, (err) => {
