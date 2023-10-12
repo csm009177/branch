e653a5cb1af2f4ac06bf5470af63e5970b622ca2
csm <csm009177@gamil.com>
2023-10-12 10:57:10 +0900

    향마루 응답처리

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const http = require('http')
 
 const server = http.createServer((req,res) => {
-
+  res.writeHead(200, {'Content-Type' : 'text/plain'});
+  res.write('향마루');
+  res.end();
 });
 
 server.listen(3500, (err) => {
