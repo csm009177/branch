e3504206e61d46d3e2600f20bdc08617c0fcc7db
csm <csm009177@gamil.com>
2023-10-12 11:35:10 +0900

    make err in err2

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 const http = require('http')
 
-const server = http.createServer((req,res) => {
-  res.writeHead(200, {'Content-Type' : 'text/plain'});
-  res.write('향마루');
-  res.end();
+
 });
 
 server.listen(3500, (err) => {
